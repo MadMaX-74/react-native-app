8e23f04ae5ff440272eb0dd986e74ca19274e4c7
maksim <m.maltsev@dsec.ru>
2024-03-28 23:11:46 +0300

    add card page

--- app/(app)/index.tsx
+++ app/(app)/index.tsx
@@ -1,30 +1,91 @@
-import { StyleSheet, View, Text } from 'react-native';
-import { Colors, Gaps } from '../../shared/tokens';
+import { View, StyleSheet, FlatList, ActivityIndicator, RefreshControl } from 'react-native';
+import { useAtomValue, useSetAtom } from 'jotai';
+import { courseAtom, loadCourseAtom } from '../../entities/course/model/course.state';
+import { useEffect } from 'react';
+import { CourseCard } from '../../widget/course/ui/CourseCard/CourseCard';
+import { StudentCourseDescription } from '../../entities/course/model/course.model';
+import { Colors } from '../../shared/tokens';
 import Button from '../../shared/Button/Button';
-import { logoutAtom } from '../../entities/auth/model/auth.state';
-import { useSetAtom } from 'jotai';
+import * as Notificaitons from 'expo-notifications';
+import * as Device from 'expo-device';
+import Constants from 'expo-constants';
 
-export default function App() {
-	const logout = useSetAtom(logoutAtom);
-	return (
-		<View style={styles.container}>
-			<View style={styles.content}>
-				<Text style={{ color: Colors.white }}>Test</Text>
-				<Button text="Logout" onPress={logout} />
+export default function MyCourses() {
+	const { isLoading, courses } = useAtomValue(courseAtom);
+	const loadCourse = useSetAtom(loadCourseAtom);
+
+	useEffect(() => {
+		loadCourse();
+	}, []);
+
+	const renderCourse = ({ item }: { item: StudentCourseDescription }) => {
+		return (
+			<View style={styles.item}>
+				<CourseCard {...item} />
 			</View>
-		</View>
+		);
+	};
+
+	const allowsNotification = async () => {
+		const settings = await Notificaitons.getPermissionsAsync();
+		return (
+			settings.granted || settings.ios?.status == Notificaitons.IosAuthorizationStatus.PROVISIONAL
+		);
+	};
+
+	const requestPermissions = async () => {
+		return Notificaitons.requestPermissionsAsync({
+			ios: {
+				allowAlert: true,
+				allowBadge: true,
+				allowSound: true,
+			},
+		});
+	};
+
+	const scheduleNotification = async () => {
+		const granted = await allowsNotification();
+		if (!granted) {
+			await requestPermissions();
+		}
+		if (Device.isDevice) {
+			const token = await Notificaitons.getExpoPushTokenAsync({
+				projectId: Constants.expoConfig?.extra?.eas.projectId,
+			});
+			console.log(token);
+		}
+	};
+
+	return (
+		<>
+			{isLoading && (
+				<ActivityIndicator style={styles.activity} size="large" color={Colors.primary} />
+			)}
+			<Button text="Напомнить" onPress={scheduleNotification} />
+			{courses.length > 0 && (
+				<FlatList
+					refreshControl={
+						<RefreshControl
+							tintColor={Colors.primary}
+							titleColor={Colors.primary}
+							refreshing={isLoading}
+							onRefresh={loadCourse}
+						/>
+					}
+					data={courses}
+					keyExtractor={(item) => item.id.toString()}
+					renderItem={renderCourse}
+				/>
+			)}
+		</>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
-		justifyContent: 'center',
-		flex: 1,
-		padding: 55,
-		backgroundColor: Colors.black,
+	item: {
+		padding: 20,
 	},
-	content: {
-		alignItems: 'center',
-		gap: Gaps.g50,
+	activity: {
+		marginTop: 30,
 	},
 });
--- shared/tokens.ts
+++ shared/tokens.ts
@@ -6,23 +6,30 @@ export const Colors = {
 	primaryHover: '#452481',
 	link: '#A97BFF',
 	grey: '#AFB2BF',
+	grayLight: '#DAD9DE',
 	white: '#FAFAFA',
 	red: '#CC384E',
+	border: '#4D5064',
+	secondary: '#E47AD5',
 };
 export const Gaps = {
 	g8: 8,
+	g10: 10,
 	g16: 16,
 	g20: 20,
 	g50: 50,
 };
 export const Radius = {
 	r10: 10,
+	r17: 17,
 };
 export const Fonts = {
+	fs12: 12,
 	fs14: 14,
 	fs16: 16,
 	fs18: 18,
 	fs20: 20,
+	fs21: 21,
 	regular: 'FiraSans-Regular',
 	semibold: 'FiraSans-SemiBold',
 };
